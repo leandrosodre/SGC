976efd7263ad3767fe876308087994093a1dedf8
leandrosodre <leandroaugustosodre@gmail.com>
2019-03-23 21:46:53 -0300

    ajustes

--- src/main/java/com/sgc/SGC/DataConfiguration.java
+++ src/main/java/com/sgc/SGC/DataConfiguration.java
@@ -24,6 +24,7 @@ public class DataConfiguration {
         basicDataSource.setUsername(username);
         basicDataSource.setPassword(password);
 
+        
         return basicDataSource;
     }
 	
