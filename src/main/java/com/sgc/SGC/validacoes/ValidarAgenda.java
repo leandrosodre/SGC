1f7ee9b411a204e6ca6b5e4c9ec849a612867a70
leandrosodre <leandroaugustosodre@gmail.com>
2019-03-24 02:02:11 -0300

    validacao agenda

--- src/main/java/com/sgc/SGC/validacoes/ValidarAgenda.java
+++ src/main/java/com/sgc/SGC/validacoes/ValidarAgenda.java
@@ -104,18 +104,6 @@ public class ValidarAgenda {
 				horaFinal.set(horaConsulta.get(Calendar.YEAR), horaConsulta.get(Calendar.MONTH), horaConsulta.get(Calendar.DATE), 
 							horario.getHoraFim(), horario.getMinutoFim(), 59);
 				
-				System.out.println("hora Consulta : " + horaConsulta.toString());
-				System.out.println("hora Inicial : " + horaInicial.toString());
-				System.out.println("hora final : " + horaFinal.toString());
-				
-				int comp1, comp2;
-				comp1 = horaConsulta.compareTo(horaInicial);
-				comp2 = horaConsulta.compareTo(horaFinal);
-				
-				System.out.println("Compare 1 : " + comp1);
-				System.out.println("Compare 2 : " + comp2);
-				
-				
 				if (horaConsulta.compareTo(horaInicial) >= 0 && horaConsulta.compareTo(horaFinal) <= 0) {
 					horarioValido = true;
 					return horarioValido;
